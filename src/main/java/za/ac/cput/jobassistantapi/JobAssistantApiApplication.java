d1039b9a323ddf78eb8c301b5ad46389ac544e43
shibambocollins <ntsobokwanec@gmail.com>
2026-04-18 01:56:54 +0200

    initial commit

---
+++ src/main/java/za/ac/cput/jobassistantapi/JobAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.jobassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssistantApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAssistantApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/cput/jobassistantapi/JobAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.jobassistantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAssistantApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
