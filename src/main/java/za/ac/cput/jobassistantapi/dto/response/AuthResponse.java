4c4d4993ea92f781e4e4d8f16014ec95745abe9b
shibambocollins <ntsobokwanec@gmail.com>
2026-04-19 15:45:50 +0200

    Create AuthResponse DTO

---
+++ src/main/java/za/ac/cput/jobassistantapi/dto/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package za.ac.cput.jobassistantapi.dto.response;
+
+public class AuthResponse {
+
+    private String message;
+
+    public AuthResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
