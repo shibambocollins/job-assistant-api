6bf598b99961e97ea3148ef2c502133a55e8f891
shibambocollins <ntsobokwanec@gmail.com>
2026-05-01 16:23:38 +0200

    Add CVService interface for CV upload functionality

---
+++ src/main/java/za/ac/cput/jobassistantapi/service/CVService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.jobassistantapi.service;
+
+import za.ac.cput.jobassistantapi.dto.request.CVUploadRequest;
+import za.ac.cput.jobassistantapi.dto.response.CVResponse;
+
+public interface CVService {
+
+    CVResponse uploadCV(CVUploadRequest request);
+}
